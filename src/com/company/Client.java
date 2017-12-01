8c8d33c9996272ae281b241fa18a1f8466dc6e74
Avgur123 <scienide@mail.ru>
2017-12-01 17:36:52 +0300

    New project

---
+++ src/com/company/Client.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+
+    Socket s;
+    BufferedReader br; // буферизировнный читатель сокета
+    BufferedWriter bw; // буферизированный писатель в сокет
+
+    public Client (String host, )
+
+}
